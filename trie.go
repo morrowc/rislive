6545532fc220ecd815ce2a28ce5da80dc19c1749
Chris Morrow <morrowc@ops-netman.net>
2019-12-07 18:20:56 -0500

    More progress in searching/lpming.

--- trie.go
+++ trie.go
@@ -10,29 +10,46 @@ import (
 
 // Tree is the binary (trie) tree which stores preefixes.
 type Tree struct {
-	Root     *Node
-	elements int
+	Root     []*Node // The first octet or nibble of an address.
+	elements int     // total number of elements stored in the tree.
 }
 
 // Node is a single tree element, with linkage to it's parent and 2 siblings.
 type Node struct {
-	Name   string
-	Prefix *net.IPNet
-	parent *Node
-	l, r   *Node
-	lock   *sync.Mutex
+	Name   string      // A nexthop.
+	Prefix *net.IPNet  // The prefix for this node.
+	parent *Node       // The node to which this node attaches.
+	l, r   *Node       // The nodes which attach to this node.
+	lock   *sync.Mutex // A mutex, to permit locking the structure if changes are to be made.
+}
+
+func (n *Node) Search(net) *net.IPNet {
+	return nil
 }
 
 func New() *Tree {
 	return &Tree{
-		Root:     &Node{},
+		Root:     make([]*Node, 256),
 		elements: 1,
 	}
 }
 
 // Lpm performs a longest prefix match in a Tree for a IPNet.
 // The match is returned or an error if there is no match.
-func (t *Tre) Lpm(n *net.IPNet) (*net.IPNet, error) {
+func (t *Tree) Lpm(n *net.IPNet) (*net.IPNet, error) {
+	if n == nil {
+		return nil, fmt.Errorf("can not LPM a nil prefix: %v", n)
+	}
+
+	// Extract the first byte/octet from prefix being searched for.
+	o := n.IP[0]
+
+	// If the first Octet exists, keep searching for the best match.
+	if node := t.Root[o]; node.Prefix != nil {
+		fmt.Printf("found first level match(%v) for: %v\n", n, o)
+		return node.Search(n)
+	}
+
 	return nil, fmt.Errorf("failed to find match for: %v", n)
 }
 
