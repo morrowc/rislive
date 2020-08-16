2fdd5d93acf6d569c67c765ad7d8221220dbb5fd
Chris Morrow <morrowc@ops-netman.net>
2020-08-16 15:30:31 -0400

    Start reformatting the trie library to work properly.

--- trie.go
+++ trie.go
@@ -1,6 +1,7 @@
 // Add a simple trie to be used in longest prefix matching for
-// ip prefixes/networks. TODO(morrowc): this should be moved to an external
-// package.
+// ip prefixes/networks.
+//
+// TODO(morrowc): this should be moved to an external package.
 // Additionally, more documentation for this library would be helpful.
 package main
 
@@ -12,45 +13,64 @@ import (
 
 // Tree is the binary (trie) tree which stores preefixes.
 type Tree struct {
-	Root     []*Node // The first octet or nibble of an address.
-	elements int     // total number of elements stored in the tree.
+	Root     *Node // The top level, least specific, prefix in the tree.
+	elements int32 // total number of elements stored in the tree.
 }
+
+// Prefix is a single Node's prefix, the IP (192.168.0.0/32) and Network (192.168.0.0/16).
+type Prefix struct {
+	IP      net.IP
+	Network *net.IPNet
+}
+
+// Convenient functions to return elements of the Prefix struct.
+func (p *Prefix) IP() net.IP      { return p.IP }
+func (p *Prefix) Net() *net.IPNet { return p.Network }
 
 // Node is a single tree element, with linkage to it's parent and 2 siblings.
 type Node struct {
 	Name   string      // A nexthop.
-	Prefix *net.IPNet  // The prefix for this node.
+	Prefix *Prefix     // The prefix information for this node, IP and Network.
 	parent *Node       // The node to which this node attaches.
 	l, r   *Node       // The nodes which attach to this node.
 	lock   *sync.Mutex // A mutex, to permit locking the structure if changes are to be made.
 }
 
-func (n *Node) Search(query *net.IPNet) *net.IPNet {
-	return nil
-}
-
-func New() *Tree {
-	return &Tree{
-		Root:     make([]*Node, 256),
-		elements: 1,
+// New creates a new tree rooted at the root prefix.
+func New(root string) (*Tree, error) {
+	ip, net, err := net.ParseCIDR(root)
+	if err != nil {
+		return nil, fmt.Errorf("parsing cidr: %v failed: %v", root, err)
 	}
+
+	return &Tree{
+		Root: &Node{Name: root,
+			Prefix: &Prefix{IP: ip,
+				Network: net},
+		},
+		elements: 1,
+	}, nil
 }
 
-// Lpm performs a longest prefix match in a Tree for a IPNet.
+// PrefixLpm implements a Longest Prefix Match for a prefix in the LPM tree.
+func (t *Tree) PrefixLpm(n *net.IPNet) (*net.IPNet, error) {
+	return t.Lpm(n.IP)
+}
+
+// Lpm performs a longest prefix match in a Tree for a net.IP.
+// Matching is done recursively down the L/R sides of each fork in the tree
+// until neither L nor R forks match the request.
+//
 // The match is returned or an error if there is no match.
-func (t *Tree) Lpm(n *net.IPNet) (*net.IPNet, error) {
+func (t *Tree) Lpm(n *net.IP) (*net.IPNet, error) {
 	if n == nil {
 		return nil, fmt.Errorf("can not LPM a nil prefix: %v", n)
 	}
 
-	// Extract the first byte/octet from prefix being searched for.
-	o := n.IP[0]
+	// Search the L/R legs of the tree, If the L and R legs this node is the match.
+	// Search down the L tree leg.
 
-	// If the first Octet exists, keep searching for the best match.
-	if node := t.Root[o]; node.Prefix != nil {
-		fmt.Printf("found first level match(%v) for: %v\n", n, o)
-		return node.Search(n), nil
-	}
+	// Search down the R tree leg.
 
 	return nil, fmt.Errorf("failed to find match for: %v", n)
 }
